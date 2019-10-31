843aa0121e31121fe231353b1323cfab917a2687
Kate <kate_goryacheva1998@mail.ru>
2019-10-31 18:00:50 +0300

    add changes

--- backup.sh
+++ backup.sh
@@ -11,7 +11,22 @@ then
 	regex="$regex$3"
 	for i in "${@:4}"; do regex="$regex$pipe$i"; done 
 	regex="$regex)"
-	find $home_dir -type f -not -path "$1/*" -regextype posix-extended -regex "^.*\.$regex\$" -exec cp -R {} "$1/" \;
+	files=$(find 2>/dev/null $home_dir -type f -not -path "$1/*" -regextype posix-extended -regex "^.*\.$regex\$")
+	for file in $files
+	do
+		file_name=$(echo $file | grep -o "[^/]*$")
+		num=1
+    		while true
+    		do
+			if [ -f "$1/($num)$file_name" ]
+			then
+				num=$(($num+1))
+			else
+				cp 2>/dev/null $file "$1/($num)$file_name"
+				break
+			fi
+		done
+	done
 	cd $1; tar -czf "$home_dir/$archive_name.tar.gz" .
 fi
 echo "done"
